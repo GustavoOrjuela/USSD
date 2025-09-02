f1ed2fb25ad694d85238e0aa2fcc826e6d700d5f
Gustavo Adolfo Orjuela Orjuela <EC3750J@comcel.com.co>
2025-09-01 19:23:28 -0500

    Base de proyecto ussd

---
+++ src/main/java/utils/UtilidadesAndroid.java
@@ -0,0 +1,25 @@
+package utils;
+
+import io.appium.java_client.android.AndroidDriver;
+import net.thucydides.core.webdriver.SerenityWebdriverManager;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UtilidadesAndroid {
+
+  public static void abrirLinkEnNavegador(String url) {
+    AndroidDriver driver = (AndroidDriver) SerenityWebdriverManager.inThisTestThread().getCurrentDriver();
+
+    Map<String, Object> intentArgs = new HashMap<>();
+    intentArgs.put("command", "am");
+    intentArgs.put("args", List.of(
+            "start",
+            "-a", "android.intent.action.VIEW",
+            "-d", url
+    ));
+
+    driver.executeScript("mobile: shell", intentArgs);
+  }
+}
