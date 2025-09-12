cfa32b5400a148fff44171a2e2923b09d17afeda
Cristian200607 <165859711+Cristian200607@users.noreply.github.com>
2025-09-12 14:22:50 -0500

    Gran parte de USSD (#1)
    Co-authored-by: CRISTIAN ANDRES PARDO ROJAS <pardoc@HILDEBOG.LOCAL>

---
+++ src/main/java/utils/PaquetesYOpcionesUSSD.java
@@ -0,0 +1,228 @@
+package utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PaquetesYOpcionesUSSD {
+
+    public static final List<String> MenuInicioUSSD = Arrays.asList(
+            "Recuerda registrar tu linea para disfrutar de los servicios Claro",
+            "1. Compra de paquetes",
+            "2. Recargas",
+            "3. Consulta de saldo y consumos",
+            "4. Activacion de linea"
+    );
+
+    public static final List<String> MenuCompraDePaquetes = Arrays.asList(
+            "1. Oferta recomendada: $12000 7 Dias 10GB+Voz",
+            "2. Paquete de Datos",
+            "3. Paquete Todo Incluido",
+            "9. Mas",
+            "0. Atras"
+    );
+
+    public static final List<String> MenuPaquetesDatos = Arrays.asList(
+            "1. $1500 1Dia 200MB Nav+WhatsApp",
+            "2. $2500 1Dia 400MB Nav+WFX",
+            "3. $4500 3Dias 800MB Nav+WFX",
+            "4. $5500 2Horas Nav Ilimitada",
+            "5. $7500 7Dias 2GB Nav+WFX",
+            "0. Atras"
+    );
+
+    public static final List<String> MenuMediosDePagoDePaqueteDeDatos = Arrays.asList(
+            "Selecciona el medio de pago para tu paquete:",
+            "1. Nequi",
+            "2. Descuento de saldo",
+            "0. Atras"
+    );
+
+    public static final List<String> MenuPaquetesTodoIncluido = Arrays.asList(
+            "1. $4500 3 Dias 400MB+WFX+Voz",
+            "2. $7500 6 Dias 1.4GB+WFX+Voz",
+            "3. $8500 7 Dias 2GB+WFX+Voz",
+            "4. $12000 7 Dias 10GB+Voz",
+            "5. $22000 20 Dias 7.5GB+WFX+Voz",
+            "9. Mas",
+            "0. Atras"
+    );
+
+    // Array estático con los textos que quieres validar
+    public static final List<String> MenuMediosDePagoPaquetesTodoIncluido = Arrays.asList(
+            "Selecciona el medio de pago para tu paquete:",
+            "1. Tarjeta de Credito",
+            "2. Nequi",
+            "3. Descuento de saldo",
+            "0. Atras"
+    );
+
+    public static final List<String> MenuMedioDePagoPSE = Arrays.asList(
+            "Selecciona el medio de pago para tu paquete:",
+            "1. Tarjeta de Credito",
+            "2. PSE",
+            "3. Nequi",
+            "4. Descuento de saldo",
+            "0. Atras"
+    );
+
+    public static final List<String> MenuPaquetesLargaDuracion = Arrays.asList(
+            "1. $12000 10 Dias 3.5GB+WFX+Voz",
+            "2. $32000 30 Dias 12GB+WFX+Voz",
+            "3. $42000 30 Dias 18GB+WFX+Voz",
+            "4. $22000 15 Dias 20GB+Voz",
+            "5. $32000 30 Dias 30GB+Voz",
+            "9. Mas",
+            "0. Atras"
+    );
+
+    public static final List<String> MenuPaquetesEspeciales = Arrays.asList(
+            "1. $100000 30 Dias 80GB+WFX+Voz",
+            "2. $8500 6 Dias 1.4GB+WFX+Salud en linea 4 beneficiarios",
+            "3. $5500 3 Dias 400MB+WFX+Salud en linea 4 beneficiarios",
+            "0. Atras"
+    );
+
+    public static final List<String> MenuPaquetesEspecialesComunidad = Arrays.asList(
+            "1. Paquete Relevo comunidad sorda",
+            "2. Paquete de Voz",
+            "0. Atras"
+    );
+
+    public static final List<String> MenuPaquetesRelevoComunidad = Arrays.asList(
+            "1. $7500 Relevo SMS Ilimitados+WFX+800MB 7Dias",
+            "2. $15500 Relevo SMS Ilimitados+WFX+2.5GB 15Dias",
+            "3. $30500 Relevo SMS Ilimitados+WFX+6GB 30Dias",
+            "0. Atras"
+    );
+
+    // Array estático con los textos que quieres validar
+    public static final List<String> MenuPaquetesApps = Arrays.asList(
+            "1. $1500 1 Dia Waze",
+            "2. $3000 1 Dia Instagram",
+            "3. $3500 1 Hora YouTube",
+            "4. $3500 7 Dias Waze.",
+            "5. $9500 15 Dias Chat WhatsApp",
+            "9. Mas",
+            "0. Atras"
+    );
+
+    // Array estático con los textos que quieres validar
+    public static final List<String> MenuPaquetesAppsMas = Arrays.asList(
+            "1. $15500 7 Dias Instagram",
+            "2. $18000 30 Dias Chat WhatsApp",
+            "0. Atras"
+    );
+
+    public static final List<String> MenuPaquetesVoz = Arrays.asList(
+            "1. $2000 1 dia 300 Minutos",
+            "2. $2500 2 dias 300 Minutos",
+            "3. $16500 20 dias 1000 Minutos",
+            "0. Atras"
+    );
+
+    public static final List<String> MenuRecargas = Arrays.asList(
+            "Compra tu recarga. Vigencia 60 dias",
+            "1. $1000",
+            "2. $2000",
+            "3. $3000",
+            "4. $4000",
+            "5. $5000",
+            "9. Mas",
+            "0. Atras"
+    );
+
+    public static final List<String> MenuMediosDePagoRecarga = Arrays.asList(
+            "Selecciona el medio de pago para tu recarga:",
+            "1. Nequi",
+            "0. Atras"
+    );
+
+    // Array estático con los textos que quieres validar
+    public static final List<String> MenuRecargasMas = Arrays.asList(
+            "Compra tu recarga. Vigencia 60 dias",
+            "1. $6000",
+            "2. $7000",
+            "3. $8000",
+            "4. $9000",
+            "5. $10000",
+            "9. Mas",
+            "0. Atras"
+    );
+
+    public static final List<String> MenuMediosDePagoRecargaConTarjeta = Arrays.asList(
+            "Selecciona el medio de pago para tu recarga:",
+            "1. Tarjeta de Credito",
+            "2. Nequi",
+            "0. Atras"
+    );
+
+    public static final List<String> MenuRecargaValoresAltos = Arrays.asList(
+            "Compra tu recarga. Vigencia 60 dias",
+            "1. $12000",
+            "2. $15000",
+            "3. $20000",
+            "4. $30000",
+            "5. $50000",
+            "0. Atras"
+    );
+
+    public static final List<String> MenuMediosDePagoRecargaConPSE = Arrays.asList(
+            "Selecciona el medio de pago para tu recarga:",
+            "1. Tarjeta de Credito",
+            "2. Nequi",
+            "3. PSE",
+            "0. Atras"
+    );
+
+    public static final List<String> MenuBeneficios3x1 = Arrays.asList(
+            "Recibe 3x1 en datos y minutos comprando con Nequi, Daviplata PSE o Tarjeta",
+            "1. Consulta de saldo y numero",
+            "2. Consulta consumo de paquete",
+            "0. Atras"
+    );
+
+    // Array estático con los textos que quieres validar
+    public static final List<String> MenuBeneficios3x1Consulta = Arrays.asList(
+            "Recibe 3x1 en datos y minutos comprando con Nequi, Daviplata PSE o Tarjeta",
+            "Linea:",
+            "Saldo:",
+            "Vigencia:",
+            "1. Compra tu paquete",
+            "2. Compra tu Recarga"
+    );
+
+    public static final List<String> MenuPrincipalPaquetes = Arrays.asList(
+            "1. Paquete de Datos",
+            "2. Paquete Todo Incluido",
+            "3. Paquete Relevo comunidad sorda",
+            "4. Paquete de Voz",
+            "0. Atras"
+    );
+
+    public static final List<String> ValidaMenuDetalleSiHayUnPaquete = Arrays.asList(
+            "Selecciona para ver detalle:",
+            "1."
+    );
+
+    public static final List<String> MenuDetalleTodoIncluidoConsumo = Arrays.asList(
+            "Vigencia del",
+            "1. Compra tu paquete",
+            "2. Compra tu Recarga"
+    );
+
+    public static final List<String> MenuAutorizacionDatos = Arrays.asList(
+            "Autorizo previa y expresamente a COMCEL S.A. para el tratamiento de mis datos personales segun la Politica de Tratamiento de la Informacion de CLARO",
+            "1. Aceptar",
+            "2. No aceptar"
+    );
+
+    public static final List<String> MenuTipoDocumento = Arrays.asList(
+            "¿Cual es tu tipo de documento? Escribe el numero de la opcion:",
+            "1. Cedula de ciudadania",
+            "2. Cedula de extranjeria",
+            "3. NIT",
+            "4. Pasaporte"
+    );
+
+
+}
